299da12443db8d890f364eeaf0742fc6123a5065
SammArt <135213913+nepsydee@users.noreply.github.com>
2024-02-17 08:34:07 +0545

    Add files via upload

---
+++ uchiha/n.js
@@ -0,0 +1,14 @@
+const audio = new Audio();
+audio.src = "./sh.mp3";
+
+const hello = new Audio();
+hello.src = "./s.mp3";
+
+const play = new Audio();
+play.src = "./i.mp3";
+
+const sound = new Audio();
+sound.src = "./o.mp3";
+
+const listen = new Audio();
+listen.src = "./u.mp3";
